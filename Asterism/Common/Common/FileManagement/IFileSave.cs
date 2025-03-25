67a5a3cef6d2f70d501e9e1206f6d7503e4e6c7f
molpheus <toru.yamaguchi109@gmail.com>
2025-03-25 21:32:07 +0900

    ファイル操作を変えれるように修正

---
+++ Asterism/Common/Common/FileManagement/IFileSave.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asterism.Common.FileManagement
+{
+    public interface IFileSave<T>
+    {
+        string FilePath { get; }
+
+        /// <summary>
+        /// ファイルに保存する
+        /// </summary>
+        /// <param name="data"></param>
+        public bool Save(T data);
+        /// <summary>
+        /// ファイルを読み込む
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Load(out T data);
+        /// <summary>
+        /// 指定のファイルが存在するか
+        /// </summary>
+        /// <returns></returns>
+        public bool Exists();
+        /// <summary>
+        /// ファイルを削除する
+        /// </summary>
+        public void Delete();
+    }
+}
---
+++ Asterism/Common/Common/FileManagement/XmlFileHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Asterism.Common.FileManagement
+{
+    public sealed class XmlFileHandler<T> : IFileSave<T>
+    {
+        public string FileName { get; }
+        public string FilePath { get; }
+
+        public XmlFileHandler(string fileName, string filePath = null)
+        {
+            filePath ??= Directory.GetCurrentDirectory();
+            FilePath = Path.Combine(filePath, fileName);
+        }
+
+        public bool Save(T data)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (var stream = new StreamWriter(FilePath))
+                {
+                    serializer.Serialize(stream, data);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public bool Load(out T data)
+        {
+            data = default;
+            if (!File.Exists(FilePath))
+                return false;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            using (var stream = new StreamReader(FilePath))
+                data = (T)serializer.Deserialize(stream);
+
+            return true;
+        }
+
+        public bool Exists() => File.Exists(FilePath);
+        public void Delete() => File.Delete(FilePath);
+    }
+
+    
+}
